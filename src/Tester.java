56b440e51dbc24b9b2c78cb898b5a5b15680d534
Dino <dino15031992@gmail.com>
2022-10-14 12:43:04 +0200

    Initial commit

---
+++ src/Tester.java
@@ -0,0 +1,17 @@
+/**
+ * @author Drumnstyle92
+ * class that contains the program.
+ */
+public class Tester {
+    /**
+     * @param args main parameter.
+     * main method that contains a for loop.
+     */
+    public static void main(String[] args) {
+        System.out.println("-------------------Start-------------------");
+     for(int i = 5; i <= 25; i++){
+         System.out.println(i);
+     }
+        System.out.println("--------------------End---------------------");
+    }
+}
